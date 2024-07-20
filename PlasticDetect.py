b6982d5b434406cdf9bfb1f755fe5a8fec17ab06
Andrés Caña <andres.intheweb@gmail.com>
2024-07-20 12:24:27 -0400

    Changes

--- PlasticDetect.py
+++ PlasticDetect.py
@@ -8,18 +8,28 @@ st.set_page_config(page_title="PlasticDetect", page_icon="🌱", layout="wide")
 with st.container():
     st.markdown("<h1 style='text-align: center;'>¡Clasifiquemos microplásticos!</h1>", unsafe_allow_html=True)
 
-# Crear dos columnas
 reto, defi, ries_con = st.columns(3)
 
-# Usar las columnas
+# Definir el estilo CSS para el color de fondo
+color_reto = "#FFD700"  # Amarillo
+color_defi = "#ADD8E6"  # Azul claro
+color_ries_con = "#90EE90"  # Verde claro
+
+# Usar las columnas con HTML y CSS para el color de fondo
 with reto:
+    st.markdown(f"<div style='background-color:{color_reto};padding:10px;border-radius:10px;'>", unsafe_allow_html=True)
     st.header("¿Qué es PlasticDetect?")
-    st.write("PlasticDetect es una aplicación que facilita la identificación y clasiﬁcación de microplásticos presentes en imágenes capturadas por los propios usuarios. Para esto utiliza una IA que se encarga de analizar las imágenes y señalar la cantidad de microplásticos presentes y su tipo. Esta aplicación es el resultado del Reto 5 del Hackaton Coafina 2024: 'Microplásticos: un desafío ciudadano'.")
+    st.write("PlasticDetect es una aplicación que facilita la identificación y clasificación de microplásticos presentes en imágenes capturadas por los propios usuarios. Para esto utiliza una IA que se encarga de analizar las imágenes y señalar la cantidad de microplásticos presentes y su tipo. Esta aplicación es el resultado del Reto 5 del Hackaton Coafina 2024: 'Microplásticos: un desafío ciudadano'.")
+    st.markdown("</div>", unsafe_allow_html=True)
 
 with defi:
+    st.markdown(f"<div style='background-color:{color_defi};padding:10px;border-radius:10px;'>", unsafe_allow_html=True)
     st.header("¿Qué son los microplásticos?")
     st.write("Los microplásticos son partículas de plástico de tamaño inferior a 5 mm que se encuentran en el medio ambiente y que son resultado de la degradación de plásticos más grandes. Estos pueden ser ingeridos por animales y afectar su salud, además de contaminar el agua y el suelo.")
+    st.markdown("</div>", unsafe_allow_html=True)
 
 with ries_con:
+    st.markdown(f"<div style='background-color:{color_ries_con};padding:10px;border-radius:10px;'>", unsafe_allow_html=True)
     st.header("Riesgos y consecuencias")
     st.write("Los microplásticos pueden tener efectos negativos en la salud de los seres vivos, ya que pueden ser ingeridos por animales y pasar a través de la cadena alimentaria. Además, pueden contaminar el agua y el suelo, afectando a los ecosistemas y a la biodiversidad.")
+    st.markdown("</div>", unsafe_allow_html=True)
