4aeb62b5c6ad999f9ce1ad05021e0a1f1e880f15
Andrés Caña <andres.intheweb@gmail.com>
2024-07-20 12:40:19 -0400

    Update PlasticDetect.py

--- PlasticDetect.py
+++ PlasticDetect.py
@@ -23,7 +23,16 @@ color_fondo_titulo_ries_con = "#004400"  # Un verde más oscuro
 
 # Usar las columnas con HTML y CSS para el color de fondo
 with reto:
-    st.markdown(f"<div style='background-color:{color_reto};padding:10px;border-radius:10px;'>", unsafe_allow_html=True)
+    st.markdown("""
+        <style>
+        .stMarkdown > div {
+            background-color: #FFD700; /* Amarillo */
+            min-height: 500px; /* Ajusta esto según tus necesidades */
+            padding: 10px;
+            border-radius: 10px;
+        }
+        </style>
+    """, unsafe_allow_html=True)
     st.markdown(f"<h2 style='text-align: center; background-color:{color_reto}; padding: 5px; border-radius: 5px;'>¿Qué es PlasticDetect?</h2>", unsafe_allow_html=True)
     st.write("PlasticDetect es una aplicación que facilita la identificación y clasificación de microplásticos presentes en imágenes capturadas por los propios usuarios. Para esto utiliza una IA que se encarga de analizar las imágenes y señalar la cantidad de microplásticos presentes y su tipo. Esta aplicación es el resultado del Reto 5 del Hackaton Coafina 2024: 'Microplásticos: un desafío ciudadano'.")
     st.markdown("</div>", unsafe_allow_html=True)
