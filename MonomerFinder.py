2f6e88035940e1a3b1284ae7711cf22b0f985f17
Andrés Caña <andres.intheweb@gmail.com>
2024-07-20 19:58:23 -0400

    Update MonomerFinder.py

--- MonomerFinder.py
+++ MonomerFinder.py
@@ -92,3 +92,16 @@ latitud = st.number_input('Latitud', value=0.0, format="%.6f")
 longitud = st.number_input('Longitud', value=0.0, format="%.6f")
 descripcion = st.text_input('Descripción de la ubicación (opcional)')
 pais = st.text_input('País')
+import folium
+import streamlit as st
+
+from streamlit_folium import st_folium
+
+# center on Liberty Bell, add marker
+m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+folium.Marker(
+    [39.949610, -75.150282], popup="Liberty Bell", tooltip="Liberty Bell"
+).add_to(m)
+
+# call to render Folium map in Streamlit
+st_data = st_folium(m, width=725)
