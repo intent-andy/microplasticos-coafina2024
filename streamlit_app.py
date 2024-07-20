6318219388b5ee9c0cd2fdbd467830d2b219cdc4
Andrés Caña <andres.intheweb@gmail.com>
2024-07-20 11:10:30 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# Set the title of the page
+st.title('Hello, Streamlit!')
+
+# Add some text
+st.write('This is my first Streamlit page.')
