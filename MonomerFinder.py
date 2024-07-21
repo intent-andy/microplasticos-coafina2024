a8fa6197e350c069806d6819e939fa15b418fdb5
Andrés Caña <andres.intheweb@gmail.com>
2024-07-21 08:21:39 -0400

    Update MonomerFinder.py

--- MonomerFinder.py
+++ MonomerFinder.py
@@ -92,10 +92,10 @@ with st.expander("Objetivos del proyecto"):
     # Imágenes de los retos de la Década Oceánica
     col1, col2 = st.columns([1, 1])
     with col1:
-        st.image("Understand-and-beat-marine-pollution.jpg", use_column_width=True)
+        st.image("Understand-and-beat-marine-pollution.webp", use_column_width=True)
 
     with col2:
-        st.image("Skills-knowledge-and-tecnology-for-all.jpg", use_column_width=True)  
+        st.image("Skills-knowledge-and-technology-for-all.webp", use_column_width=True)  
     
     st.markdown("</div>", unsafe_allow_html=True)
 
