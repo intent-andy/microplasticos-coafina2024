5f09e31add9505c11f764a6498faf6c1da2ea2d6
Andrés Caña <andres.intheweb@gmail.com>
2024-07-21 00:08:36 -0400

    Update MonomerFinder.py

--- MonomerFinder.py
+++ MonomerFinder.py
@@ -12,7 +12,7 @@ banner_image = Image.open("Microplastics_in_the_natural_environment.jpg")
 banner_image_rotated = banner_image.rotate(90, expand=True)
 
 # Mostrar la imagen del banner
-st.image(banner_image_rotated, width=700)
+st.image(banner_image_rotated, width=200)
 
 
 # Definir el estilo CSS para el color de fondo
